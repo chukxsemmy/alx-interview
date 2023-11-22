fd8fddfb149b95b6922c001b83449b714207158b
chukxsemmy <chukxsemmy@gmail.com>
2023-11-22 09:56:48 -0800

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Task: Change comes from within
+"""
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins
+    needed to meet a given amount total
+    """
+
+    if total <= 0:
+        return 0
+    min_coins = [0] + [float('inf')] * total
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                num_coins = min_coins[i - coin] + 1
+                if num_coins < min_coins[i]:
+                    min_coins[i] = num_coins
+    if min_coins[-1] == float("inf"):
+        return -1
+    return min_coins[total]
